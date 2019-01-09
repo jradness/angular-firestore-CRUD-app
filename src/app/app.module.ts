728a2c43abbca629ee0ce73964d522e9ed8209ad
Jason Brewer <jason@devslopes.com>
2019-01-09 15:29:33 -0600

    updated app module with prod keys

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { environment} from '../environments/environment';
+import { environment} from '../environments/environment.prod';
 import { AngularFireModule} from 'angularfire2';
 import { AngularFirestoreModule } from 'angularfire2/firestore';
 import { FormsModule } from '@angular/forms';
